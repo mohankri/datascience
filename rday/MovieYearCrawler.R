abd1305342c7ba15695550020400d5b121400f22
mohankri <mohankri@gmail.com>
2017-03-12 21:35:27 -0700

    Update WebCrawler for Movie Year 1915-2023

--- rday/MovieYearCrawler.R
+++ rday/MovieYearCrawler.R
@@ -1,16 +1,42 @@
+
+if (!require(XML)) install.packages('XML')
+
 library(XML)
 library(plyr)
-DIR <- "./"
-setwd(DIR)
+DIR <- "~/datascience/rday/"
+setwd(dir=DIR)
 
+#
+# Extract data for each individual year
+#
+data_url <- function(year, source) {
+	url <- paste(source)
+        # get the data frame
+	yrtbl <- readHTMLTable(url, which=1)
+	
+	finalData <- subset(yrtbl, !(is.na(yrtbl$Movie) & is.na(yrtbl$ProductionBudget) &
+	                      is.na(yrtbl$DomesticBox)))
+	file_name<-paste("movie_year_", year, ".csv", sep="")
+	print(file_name)
+	write.table(finalData, file_name, sep=",", row.names=F)
+}
+
+#
+# For each year found to call check_url to tranverse movie released in that 
+# particular year.
+#
 
 check_url <- function(year) {
 	yearurl <- paste("http://www.the-numbers.com/movies/year/", year, sep="")
-	print(yearurl)
+	print(paste("Dump data for year", yearurl))
+	data_url(year, yearurl)
 }
 
+#
+# Read the table and create a sorted order set of data for each year
+#
 MovieYearCrawler <- function(source) {
-	url <- paste("http://www.the-numbers.com/movies/#tab=year")
+	url <- paste(source)
         # get the data frame
 	tables <- readHTMLTable(url, which=1)
 	#select the year column of dataframe
@@ -19,4 +45,5 @@ MovieYearCrawler <- function(source) {
 	lapply(yeardata[position], check_url)
 }
 
-MovieYearCrawler("movielist")
+# Call to parse the main HTML Page.
+MovieYearCrawler("http://www.the-numbers.com/movies/#tab=year")
